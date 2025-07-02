f68f20fd1c4fe4e3a876ff9f6beb5ff08993eb6a
Ezi0 <cenx@tuta.io>
2025-07-02 23:55:11 +0200

    Vault added

---
+++ DencryptCore/vault.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DencryptCore
+{
+    public static class Vault
+    {
+        
+
+    }
+}
